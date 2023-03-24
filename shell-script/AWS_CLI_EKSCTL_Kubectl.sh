b6b790532b2f7ed9690e9dadae73ca0e887dbeec
krushna1512 <krishna.sharma042@gmail.com>
2023-03-24 21:25:22 +0530

    Index file change4

---
+++ shell-script/AWS_CLI_EKSCTL_Kubectl.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#Install AWS CLI
+yum update -y
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+./aws/install -i /usr/local/aws-cli -b /usr/local/bin
+
+#Install Kubectl
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.24.10/2023-01-30/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+kubectl version --short --client
+
+#Install Eksctl
+yum install git -y
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+export PATH=$PATH:/usr/local/bin
+echo 'export PATH=$PATH:/usr/local/bin' >> ~/.bashrc
+eksctl version
